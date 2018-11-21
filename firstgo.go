7f0d1daff1855ba198a7519b50aabcfa8dbaa223
viveksingggits <vivek@infracloud.io>
2018-11-21 16:42:40 +0530

    workflow working for dummy hedaers

---
+++ firstgo.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "net/textproto"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+    // recommend
+    v1 := r.Header.Get("HEADER_KEY_1")
+
+    // need to convert string cases manually
+    key := textproto.CanonicalMIMEHeaderKey("HEADER_KEY_2")
+    // v2 type is []string
+    v2, _ := r.Header[key]
+
+    w.Write([]byte(fmt.Sprintf("v1: %s, v2: %v", v1, v2)))
+}
